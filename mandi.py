5e0dc9e34ece941f1a416da8e82aa42ed756a48a
Naman Chopra <102227521+Naman-chopra@users.noreply.github.com>
2022-11-12 17:25:38 +0530

    Added updated scraping code
    scraping code with runtime variable support. Support for in chat replies and mandi rates on the way

--- mandi.py
+++ mandi.py
@@ -165,3 +165,57 @@ def market_get(ans, context, update, lang):
     #     update.message.reply_text("no data found")
 # https://www.commodityonline.com/mandiprices/mango
 
+'''
+TESTING CODE BELOW
+CHANGE CHAT ID AND TOKEN
+CURRENT SUPPORT FOR RUNTIME INPUT NOT THROUGH CHAT INTERFACE
+
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import datetime as dt
+from googletrans import Translator
+
+def prices(ans,update,context):
+    l=ans.split(', ')
+    state=l[0].lower()
+    crop=l[1].lower()
+# ###"My name is {fname}, I'm {age}".format(fname = "John", age = 36)
+    # ###req="https://www.commodityonline.com/mandiprices/{crop}/{state}".format(crop=input("Enter the crop: "),state=input("Enter the state: "))
+    req="https://www.commodityonline.com/mandiprices/{crop}/{state}".format(crop=crop,state=state)
+
+    hit=requests.get(req)
+    soup=BeautifulSoup(hit.text,"lxml")
+    table=soup.find('table',{'main-table2'})
+    headers=[]
+    for i in table.find_all('th'):
+        title=i.text.strip()
+        headers.append(title)
+    ### headers.remove('Telegram')
+
+    df=pd.DataFrame(columns=headers)
+    for i in table.find_all('tr')[1:]:
+        data=i.find_all('td')
+        rowdata=[td.text.strip() for td in data]
+        length=len(df)
+        df.loc[length]=rowdata
+    ### df.drop(['Telegram'],axis=1)
+    delcol=['Telegram', 'Max Price', 'Min Price', 'State', 'Commodity']
+    df=df.drop(columns=delcol,axis=1)
+    ### Uncomment code below to save data tp text file
+    ## df.to_csv('mandi_data.txt', sep ='\t')
+
+    print(df)
+    return df
+    #update.message.reply_text(df)
+result = prices().to_string(index = False)
+
+def send_msg(text):
+   token = "5639336072:AAEvrJpaiLmRGZ5r3lNbo97iitGUMYPfoX4"
+   chat_id = "your chat id"
+   url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + chat_id + "&text=" + text 
+   results = requests.get(url_req)
+#    print(results.json())
+
+send_msg(result)
+'''
